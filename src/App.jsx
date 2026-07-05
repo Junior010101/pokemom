1d531c2405936a13ba7bb9f0c3d5d811c2de5f65
junior010101 <marcondesjunior2209@gmail.com>
2026-07-04 21:04:40 -0300

    add: hitbox map

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,64 @@
 import { useEffect, useState, useRef } from "react";
-import "./index.css";
-import movim from "./scripts/movimento.js";
+
 import { Map } from "./scenes/nuvema_town/Map";
 import { Player } from "./components/Player";
 
+import movim from "./scripts/movimento.js";
+import "./index.css";
+
 function App() {
   const cameraRef = useRef(null);
+
+  const TILE_SIZE = 16;
+  const SCALE = 3;
 
   const [mapDimensions, setMapDimensions] = useState({
     widthPx: 0,
     heightPx: 0,
   });
-
-  const [characterPosition, setCharacterPosition] = useState({
+  const [cameraSize, setCameraSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  const [playerPosition, setPlayerPosition] = useState({
     x: 14,
     y: 18,
   });
-    const [cameraSize, setCameraSize] = useState({
-    width: 0,
-    height:0,
-  });
+
   useEffect(() => {
-    if (cameraRef.current){
+    if (cameraRef.current) {
       setCameraSize({
         width: cameraRef.current.clientWidth,
-        height:cameraRef.current.clientHeight,
-      })
+        height: cameraRef.current.clientHeight,
+      });
     }
+
+    movim(setPlayerPosition);
   }, []);
 
-  // Use a state or ref to track viewport if it needs to update on resize
+  const visualX = Math.max(
+    Math.min(
+      -(
+        playerPosition.x * TILE_SIZE +
+        TILE_SIZE / 2 -
+        cameraSize.width / (2 * SCALE)
+      ),
+      0,
+    ),
+    -(mapDimensions.widthPx - cameraSize.width / SCALE),
+  );
 
-  const TILE_SIZE = 16;
-  const SCALE = 3;
-
-  // Calculate visual positions outside of useEffect so they are available to JSX
-  const visualX = Math.max(Math.min(-(characterPosition.x * TILE_SIZE +TILE_SIZE / 2 -cameraSize.width / (2 * SCALE)),0),-(mapDimensions.widthPx - cameraSize.width / SCALE));
-
-  const visualY = Math.max(Math.min(-(characterPosition.y * TILE_SIZE +TILE_SIZE / 2 -cameraSize.height / (2 * SCALE)),0),
-  -(mapDimensions.heightPx - cameraSize.height / SCALE)
-);
-
-  useEffect(() => {movim(setCharacterPosition);}, []);
+  const visualY = Math.max(
+    Math.min(
+      -(
+        playerPosition.y * TILE_SIZE +
+        TILE_SIZE / 2 -
+        cameraSize.height / (2 * SCALE)
+      ),
+      0,
+    ),
+    -(mapDimensions.heightPx - cameraSize.height / SCALE),
+  );
 
   return (
     <div className="GameCamera" ref={cameraRef}>
@@ -50,13 +67,13 @@ function App() {
         style={{
           width: `${mapDimensions.widthPx}px`,
           height: `${mapDimensions.heightPx}px`,
-          // Ensure you use the variables calculated above
           transform: `scale(${SCALE}) translate3d(${visualX}px, ${visualY}px, 0)`,
           transformOrigin: "top left",
-        }}>
+        }}
+      >
         <Player
-          posX={characterPosition.x}
-          posY={characterPosition.y}
+          posX={playerPosition.x}
+          posY={playerPosition.y}
           tileSize={TILE_SIZE}
         />
         <Map tileSize={TILE_SIZE} onMapLoaded={setMapDimensions} />
